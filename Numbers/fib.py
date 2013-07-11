c37027e00c4c81bb8e7c3ac9c7828ed913a157eb
Adam Taylor <aktaylor08@gmail.com>
2013-07-10 19:35:57 -0500

    fibanaci using a generator

--- Numbers/fib.py
+++ Numbers/fib.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 import sys
 
-def fib(n):
+def fib():
     '''calculate the fibanatci sequence up to the nth number'''
     count = 0
     b, a = 0,1
-    while count < n:
-        print a
+    while 1:
+        yield a
         temp = a+b
         b = a
         a = temp
-        count += 1
         
 if __name__ == '__main__':
-    print sys.argv
-    fib(int(sys.argv[1]))
+    n = int(sys.argv[1])
+    a = fib()
+    for i in range(n):
+        print a.next()
 
